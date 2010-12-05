472116249b9f0f93030c319c93fb76b50570d3ff
Kevin <kquiring@gmail.com>
2010-12-05 16:31:42 -0600

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Achievement::Application.routes.draw do
 	resources :users
 	
   #as :user do
-		#get "/" => "devise/sessions#new"
+		#root :to => "users#show"
 	#end
 	
 	#namespace :user do
@@ -60,7 +60,7 @@ Achievement::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "users#show"
+  root :to => "users#new"
 
   # See how all your routes lay out with "rake routes"
 
