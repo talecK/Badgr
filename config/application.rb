ba6ceba0e47e15ce66dc75dcaabb6a698460f1f2
Kevin <kquiring@gmail.com>
2011-01-25 20:50:28 -0600

    removed caching js files in application.erb (heroku is read only! doh0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 module Badgr
   class Application < Rails::Application
     config.active_record.observers = :membership_observer, :achievement_observer
-    ActiveRecord::Base.include_root_in_json = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
