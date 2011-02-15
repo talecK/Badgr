61f413c010ae55de80fb585765f0eb6aab95a840
Kevin <kquiring@gmail.com>
2011-02-15 17:18:17 -0600

    continuing to work on requesting achievements

--- app/controllers/user_achievements_controller.rb
+++ app/controllers/user_achievements_controller.rb
@@ -1,7 +1,19 @@
 class UserAchievementsController < ApplicationController
+	before_filter :authenticate_user!
+
   def new
   end
+
   def create
+    @achievement = Achievement.find( params[:achievement_id] )
+
+    if( @achievement != nil && current_user.achievements.create( :achievement_id => @achievement.id ) )
+      flash[:notice] = "A request for the test_achievement has been sent to the officers of the #{@achievement.group.name} Hub."
+    else
+      flash[:error] = "Could not send a request for that achievement."
+    end
+
+    redirect_to group_achievements_path(@achievement.group)
   end
 end
 
